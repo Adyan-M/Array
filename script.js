d6f4341cb6a4a2a964f433a7233446368069a537
Adyan-M <adyan.muntasir@gmail.com>
2025-03-24 21:58:21 +0600

    Array

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ let fruit = {
     color: "Orange-red",
     countries:   ["Pakistan","Sri Lanka", "India","Bangladesh","Thailand"], 
 }
-
 document.getElementById("p1").innerHTML = fruit.name;
 document.getElementById("p2").innerHTML =  fruit.countries[3];
