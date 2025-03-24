82f21be3701f520cdf0ef32118eaac0e26a32dac
Adyan-M <adyan.muntasir@gmail.com>
2025-03-24 21:56:37 +0600

    Homework

---
+++ script.js
@@ -0,0 +1,8 @@
+let fruit = {
+    name: "Mango",
+    color: "Orange-red",
+    countries:   ["Pakistan","Sri Lanka", "India","Bangladesh","Thailand"], 
+}
+
+document.getElementById("p1").innerHTML = fruit.name;
+document.getElementById("p2").innerHTML =  fruit.countries[3];
